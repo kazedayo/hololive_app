f06af99059baf4d04590825810afdb5dcd277d32
kazedayo <winsonlam@me.com>
2021-05-02 21:23:19 +0800

    periodic timer for ato update

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,7 +8,28 @@ import 'package:hololive_app/home/widgets/video_card.dart';
 import 'package:hololive_app/models/stream_video_item/stream_video_item.dart';
 import 'package:lottie/lottie.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Timer _refreshTimer;
+
+  @override
+  void initState() {
+    _refreshTimer = Timer.periodic(Duration(minutes: 1), (timer) {
+      BlocProvider.of<HomePageBloc>(context).add(RequestLiveList());
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _refreshTimer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     BlocProvider.of<HomePageBloc>(context).add(RequestLiveList());
